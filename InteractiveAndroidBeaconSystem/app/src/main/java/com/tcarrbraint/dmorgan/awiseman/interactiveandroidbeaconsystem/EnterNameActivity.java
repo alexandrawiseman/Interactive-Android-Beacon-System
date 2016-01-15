235fc380b63238abacaff66037ee9043b91d1b52
awiseman <0aw34@queensu.ca>
2016-01-15 14:46:57 -0500

    Modified enter name screen
    -Added check to make sure valid name is entered
    -Cleaned up code

--- InteractiveAndroidBeaconSystem/app/src/main/java/com/tcarrbraint/dmorgan/awiseman/interactiveandroidbeaconsystem/EnterNameActivity.java
+++ InteractiveAndroidBeaconSystem/app/src/main/java/com/tcarrbraint/dmorgan/awiseman/interactiveandroidbeaconsystem/EnterNameActivity.java
@@ -5,21 +5,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class EnterNameActivity extends Activity {
+
+    private EditText mNameEditText;
+    private Button mSubmitName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_name);
 
-        Button submitname = (Button) findViewById(R.id.submitname);
-        submitname.setOnClickListener(new View.OnClickListener() {
+        mNameEditText = (EditText) findViewById(R.id.name_editText);
+
+        mSubmitName = (Button) findViewById(R.id.submitname);
+        mSubmitName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Bluetooth operated menu selects game to play
-                Intent pickerIntent = new Intent(EnterNameActivity.this, GamePickerActivity.class);
-                startActivity(pickerIntent);
+                if(!mNameEditText.getText().toString().matches(""))
+                {
+                    //Bluetooth operated menu selects game to play
+                    Intent pickerIntent = new Intent(EnterNameActivity.this, GamePickerActivity.class);
+                    startActivity(pickerIntent);
+                }
             }
         });
 
