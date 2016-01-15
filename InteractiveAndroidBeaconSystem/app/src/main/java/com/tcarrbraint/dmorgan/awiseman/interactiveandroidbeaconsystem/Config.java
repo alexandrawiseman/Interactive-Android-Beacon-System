fd1fa37d0fe82658f7fbd7860a962cc17faae646
DanielJMorgan <danieljosephmorgan@live.ca>
2016-01-15 15:11:08 -0500

    Store Student Name In Database
    Added Config.java
    Added Request Handler.java
    Modified EnterNameActivity

---
+++ InteractiveAndroidBeaconSystem/app/src/main/java/com/tcarrbraint/dmorgan/awiseman/interactiveandroidbeaconsystem/Config.java
@@ -0,0 +1,30 @@
+package com.tcarrbraint.dmorgan.awiseman.interactiveandroidbeaconsystem;
+
+/**
+ * Created by Dan on 14/01/2016.
+ */
+public class Config {
+
+    //Address of our scripts of the CRUD
+    public static final String URL_ADD="http://interactivebluetooth.ddns.net/Android/498_Project/addEmp.php";
+    public static final String URL_GET_ALL = "http://interactivebluetooth.ddns.net/Android/498_Project/getAllEmp.php";
+    public static final String URL_GET_EMP = "http://interactivebluetooth.ddns.net/Android/498_Project/getEmp.php?id=";
+    public static final String URL_UPDATE_EMP = "http://interactivebluetooth.ddns.net/Android/498_Project/updateEmp.php";
+    public static final String URL_DELETE_EMP = "http://interactivebluetooth.ddns.net/Android/498_Project/deleteEmp.php?id=";
+
+    //Keys that will be used to send the request to php scripts
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAME = "name";
+    public static final String KEY_EMP_DESG = "loc";
+    public static final String KEY_EMP_SAL = "score";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_DESG = "loc";
+    public static final String TAG_SAL = "score";
+
+    //employee id to pass with intent
+    public static final String EMP_ID = "emp_id";
+}
--- InteractiveAndroidBeaconSystem/app/src/main/java/com/tcarrbraint/dmorgan/awiseman/interactiveandroidbeaconsystem/EnterNameActivity.java
+++ InteractiveAndroidBeaconSystem/app/src/main/java/com/tcarrbraint/dmorgan/awiseman/interactiveandroidbeaconsystem/EnterNameActivity.java
@@ -1,23 +1,37 @@
 package com.tcarrbraint.dmorgan.awiseman.interactiveandroidbeaconsystem;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class EnterNameActivity extends Activity {
+import java.util.HashMap;
+
+public class EnterNameActivity extends Activity{
+
+    private EditText editTextName;
+    private EditText editTextLoc;
+    private EditText editTextScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_name);
 
+        editTextName = (EditText) findViewById(R.id.editText);
+
         Button submitname = (Button) findViewById(R.id.submitname);
         submitname.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Bluetooth operated menu selects game to play
+                addStudent();
+                //GetID
                 Intent pickerIntent = new Intent(EnterNameActivity.this, GamePickerActivity.class);
                 startActivity(pickerIntent);
             }
@@ -30,8 +44,44 @@ public class EnterNameActivity extends Activity {
                 finish();
             }
         });
+    }
 
+    private void addStudent(){
 
+        final String name = editTextName.getText().toString().trim();
+
+        class AddStudent extends AsyncTask<Void,Void,String>
+        {
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(EnterNameActivity.this,"Adding...","Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(Config.KEY_EMP_NAME,name);
+                params.put(Config.KEY_EMP_DESG,"Z");
+                String put = params.put(Config.KEY_EMP_SAL, "0");
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Config.URL_ADD, params);
+                return res;
+            }
+        }
+
+        AddStudent ae = new AddStudent();
+        ae.execute();
     }
 
 }
